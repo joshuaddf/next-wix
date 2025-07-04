84696d4ce35d5688931fe428fb49f49115b69701
joshuaddf <jkdodofoli@gmail.com>
2025-07-04 00:59:20 +0000

    Added some content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="font-geist-mono flex items-center justify-center flex-col h-screen">
-      <div className="flex flex-col items-start">
+      <div className="flex flex-col items-start px-6">
       <h1 className=" text-xl">This is being made with wix cms</h1>
       <p className="py-6">Wanna see some content? Checkout <Link className="font-bold" href="/blog">blog </Link>for some content. 😉</p>
       </div>
